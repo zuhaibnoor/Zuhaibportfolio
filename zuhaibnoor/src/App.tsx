0a8d5361869766a3d8aff401a6c45a00043e6f13
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-02-09 20:29:00 +0500

    updated about section

--- zuhaibnoor/src/App.tsx
+++ zuhaibnoor/src/App.tsx
@@ -27,10 +27,18 @@ function App() {
 
         {/* About me start */}
 
-        <div className='h-screen bg-[#00000D] p-10'>
-            <div className='bg-[#171716] m-auto w-[70%] h-full p-10'>
+        <div className='h-screen bg-[#00000D] p-2 md:p-10 sm:h-[80vh] md:h-[90vh]'>
+            <div className='bg-[#171716] m-auto w-[95%] sm:w-[70%] h-full p-10  rounded-lg'>
                 <h1 className='text-white text-center font-bold text-[50px]'>{"< "}About <div className=' inline text-[#7ABA78]'>Me</div>{" />"}</h1>
-                <p className='text-center text-white'></p>
+                <p className='text-center text-white text-[20px] pt-10 text-center tracking-wide leading-[2] md:leading-[3] family-courier'>Code, creativity, and curiosity—that’s my world.
+                     As a Computer Systems Engineering student, <br/>I love building smart solutions, whether it’s crafting sleek software or optimizing systems. Always exploring, always learning—let’s create something awesome! 🚀</p>
+
+                <div className='text-center pt-10'>
+                    <a href="./src/assets/zuhaibs-resume.pdf" download className='bg-[#7ABA78] text-white font-bold py-2 px-4 rounded italic'>
+                        My Resume!
+                    </a>
+                </div>
+
             </div>
         </div>
 
--- zuhaibnoor/src/components/Navbar.tsx
+++ zuhaibnoor/src/components/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Navbar() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <img
                   className="h-10 w-10 rounded-full"
-                  src="./public/profile.png"
+                  src="./profile.png"
                   alt="Your Profile"
                 />
               </div>
