2460eb4bf348f8ff0f800603594e4a6899a3c852
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-02-10 13:41:35 +0500

    updated responsiveness in about section

--- zuhaibnoor/src/App.tsx
+++ zuhaibnoor/src/App.tsx
@@ -27,10 +27,10 @@ function App() {
 
         {/* About me start */}
 
-        <div className='h-screen bg-[#00000D] p-2 md:p-10 sm:h-[80vh] md:h-[90vh]' id='about'>
+        <div className='bg-[#00000D] p-2 md:p-10 sm:h-[60vh] md:h-[110vh] lg:h-[70vh]' id='about'>
             <div className='bg-[#171716] m-auto w-[95%] sm:w-[70%] h-full p-5 md:p-10  rounded-lg'>
                 <h1 className='text-white text-center font-bold text-[40px] md:text-[50px]'>{"< "}About <div className=' inline text-[#7ABA78]'>Me</div>{" />"}</h1>
-                <p className='text-center text-white text-[20px] pt-10 text-center tracking-wide leading-[2] md:leading-[3] family-courier md:pt-3'>Code, creativity, and curiosity—that’s my world.
+                <p className='text-center text-white text-[20px] pt-10 text-center tracking-wide leading-[2] md:leading-[3] lg:leading-[2] family-courier md:pt-3'>Code, creativity, and curiosity—that’s my world.
                      As a Computer Systems Engineering student, <br/>I love building smart solutions, whether it’s crafting sleek software or optimizing systems. Always exploring, always learning—let’s create something awesome! 🚀</p>
 
                 <div className='text-center pt-10'>
